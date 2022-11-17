c9690ff239a938b078259b26e7badd6ef957aff2
kileyhudson <kileyhudson@10.232.137.116>
2022-11-17 14:15:08 -0600

    gf

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -3,8 +3,40 @@ package studio8;
 public class Date {
 
 
-    public static void main(String[] args) {
+   
+   private int month; 
+   private int day;
+   private int year;
+   
+   // constructor
+  
+   /**
+    * constructor that adds values to instance variables
+    * @param month
+    * @param day
+    * @param year
+    */
+   public Date(int month, int day, int year) {
+	   this.month= month;
+	   this.day= day;
+	   this.year= year;
+   }
+   
 
+    public String toString() {
+    	return this.month + "/" + this.day + "/" + this.year;
     }
+   // equals() 
 
+
+  //  hashCode()
+    public void setMonth(int month) {
+    	this.month= month;
+    }
+    
+    
+ public static void main(String[] args) {
+   Date date1 = new Date(5,3,2002);
+   System.out.print(date1);
+ }
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -3,9 +3,51 @@ package studio8;
 
 public class Time {
 
-	public static void main(String[] args) {
- 
-    	
-    }
+	
+private int hour;
+private int minute;
+private boolean militaryTime;
+	
+// method that determines how toString should be for how the time should be displayed
+// 12- vs 24- hour time
+
+/**
+ * constructor that sets values to instance variables
+ * military time influences how the hour will appear
+ * @param hour
+ * @param minute
+ */
+public Time(int hour, int minute, boolean militaryTime){
+	this.militaryTime=militaryTime;
+	if (militaryTime ) {
+		this.hour = hour;
+
+	} else {
+		this.hour = hour/2;
+	}
+	this.minute= minute;
+}
+
+public String toString() {
+	return this.hour +":"+ this.minute;
+}
+
+public void setHour(int hour) {
+	this.hour=hour;
+}
+//equals() 
+
+
+//hashCode()
+
+public void main(String[] args) {
+	//Time(5, 30, false);
+	//	minute =30;
+	//	militaryTime = false;
+	setHour(5);
+
+	System.out.println(hour);
+	
+}
 
 }
